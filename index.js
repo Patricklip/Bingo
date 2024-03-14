e4242b9dc762941a74f394acc39c013be8e638bf
Patrick Lipana <lipanapatrick58@gmail.com>
2024-03-14 14:52:17 +0800

    second commit

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
 console.log("Hello World!");
 
-generateNumber (15);
+generateNumber(75);
+
 
 function generateNumber(max) {
+
     let randomNumber = Math.floor(Math.random() * max) + 1;
 
-
-
-    if(randomNumber >= 15) {
-        console.log('The number is in B.');
+    if (randomNumber <= 15) {
+        console.log("The number is in B.");
     } else if (randomNumber >= 16 && randomNumber <= 30) {
-        console.log('The number is in I.');
-    } else if (randomNumber >= 31 && randomNumber <= 30) {
-        console.log('The number is in N.');
-    } else if (randomNumber >= 46 && randomNumber <= 30) {
-        console.log('The number is in G.');
-    } else if (randomNumber >= 61 && randomNumber <= 30) {
-        console.log('The number is in O.');
+        console.log("Then number is in I.")
+    } else if (randomNumber >= 31 && randomNumber <= 45) {
+        console.log("The number is in N.");
+    } else if (randomNumber >= 46 && randomNumber <= 60) {
+        console.log("The number is in G.");
+    } else if (randomNumber >= 61 && randomNumber <= 75) {
+        console.log("The number is in O.");
     } else {
-        console.log('The number is invalid');
-    } 
+        console.log("The number is invalid!");
+    }
 
     return console.log(randomNumber);
+
 }
