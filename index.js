957e0e4f600c5b3e4bb7bd1bdfa3e9636233c657
Patrick Lipana <lipanapatrick58@gmail.com>
2024-03-19 14:22:24 +0800

    Added a new random number generator

--- index.js
+++ index.js
@@ -1,26 +1,63 @@
-console.log("Hello World!");
+console.log("Hello World!")
 
-generateNumber(75);
+let B = false, I = false, N = false, G = false, O = false;
+
+let BNum, INum, NNum, GNum, ONum;
+
+function newNumber (maxNumber){
+let x = Math.floor(Math.random() * maxNumber) + 1;
 
 
-function generateNumber(max) {
+if(x <= 15){
+    console.log('New number is ${x} belongs to B');
+    B=true;
+    BNum = x;
+} else if (x >= 16 && x <= 30 ){
+    console.log ('New number is ${x} belongs to I');
+    I= true;
+    INum = x;
+} else if (x >= 31 && x <=45 ) {
+    console.log ('New number is ${x} belong to N');
+    N=true;
+    NNum = x;
+} else if ( x >= 46 && x  <= 60){
+    console.log ('New number is ${x} belongs to G');
+    G=true;
+    GNum = x;
+}else if ( x >=  61 && x <= 75){
+    console.log ('New Number is ${x} belongs to 0');
+    O=true;
+    ONum = x;
+}else{
+    console.log('New number ${x} is invalid!');   
+}
 
-    let randomNumber = Math.floor(Math.random() * max) + 1;
+if (B = true && I == true && N == true && G == true && O == true){
+    console.log('BINGOOOO!!!!');
+    console.table({
+        B: BNum,
+        I: INum,
+        N: NNum,
+        G: GNum,
+        O: ONum,
+})
 
-    if (randomNumber <= 15) {
-        console.log("The number is in B.");
-    } else if (randomNumber >= 16 && randomNumber <= 30) {
-        console.log("Then number is in I.")
-    } else if (randomNumber >= 31 && randomNumber <= 45) {
-        console.log("The number is in N.");
-    } else if (randomNumber >= 46 && randomNumber <= 60) {
-        console.log("The number is in G.");
-    } else if (randomNumber >= 61 && randomNumber <= 75) {
-        console.log("The number is in O.");
-    } else {
-        console.log("The number is invalid!");
-    }
+}
 
-    return console.log(randomNumber);
+return console.log(x);
+}
 
+//newNumber(75);
+
+let counter = 5;
+//for (counter = 10; counter >= 0; counter--){
+//    console.log(Current counter is ${counter});
+//    if (counter == 2 )break;
+
+//    newNumber(75);
+//}
+
+
+while (B = false || I == false || N == false || G == false  || O == false){
+    newNumber(75);
 }
